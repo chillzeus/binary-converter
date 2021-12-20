f236bc589ec898aaa7152f7fab1ca0379bcc3e6d
ChillZeus <fiedlerisaac1@gmail.com>
2021-12-19 18:21:39 -0600

    idk man

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-const binary = ["1", "1", "0", "0"];
+const binary = ["1", "1", "0", "0", "1", "1"];
 let addTo = 0.5;
 let sum = 0;
 
-for (let i = 0; i < binary.length; i++) {
-	addTo = addTo * 2;
+for (let i = 0; i < binary.length; i++) { 
+  addTo = addTo * 2;
   console.log(addTo)
+}
+
+for (let i = 0; i < 10; i++) {
+  addTo = addTo * 2;
+  //console.log(i)
   if (binary[i] == "1") {
     sum = sum + addTo;
-    console.log(sum)
+    //console.log(sum)
   }
 }
