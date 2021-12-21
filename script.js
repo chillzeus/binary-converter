2a1d3c9acb09be063d1e6fc01c6969c331b3d075
ChillZeus <76137066+ChillZeus@users.noreply.github.com>
2021-12-21 08:21:08 -0600

    I mean this works

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const binary = ["1", "1", "0", "0", "1", "1"];
+const binary = ["1", "1", "1", "1", "1", "1"];
 let addTo = 0.5;
 let sum = 0;
 
 for (let i = 0; i < binary.length; i++) { 
   addTo = addTo * 2;
-  console.log(addTo)
+  //console.log(addTo)
 }
 
 for (let i = 0; i < 10; i++) {
-  addTo = addTo * 2;
-  //console.log(i)
   if (binary[i] == "1") {
+  	//console.log(addTo)
     sum = sum + addTo;
-    //console.log(sum)
+    console.log(sum)
   }
+  addTo = addTo / 2;
 }
